f36aa7019710273cd8b0987469603960e59aef81
jasonwangcode <111999998+jasonwangcode@users.noreply.github.com>
2022-08-24 11:28:59 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*
+Hello.java
+Jason Wang
+08/24/22
+CMSC 255 004
+*/
+
+public class Hello {
+	public static void main(String[] args){
+		System.out.println("Jason Wang");
+		System.out.println("Hello.java");
+		System.out.println("This program displays my name.");
+		System.out.println("08/24/22");
+		System.out.println("CMSC 255 004");
+		System.out.println();
+	}
+}
